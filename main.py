ad325fab0a4db6853df26ee9047801f151017847
TheSnoozer <s.lange@alumni.tu-berlin.de>
2023-04-26 22:28:08 +0200

    prettify code

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ from requests.adapters import HTTPAdapter
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-s = requests.Session()
-s.mount('https://', HTTPAdapter(max_retries=5))
-
-
 def endpoint(part):
     return 'https://{base}/{part}?_format=json'.format(
         base='radars.securite-routiere.gouv.fr/radars',
@@ -21,8 +17,8 @@ def endpoint(part):
     )
 
 
-def do_request(url):
-    r = s.get(
+def do_request(session, url):
+    request = session.get(
         url,
         verify=False,
         headers={
@@ -41,23 +37,25 @@ def do_request(url):
             'sec-ch-ua-platform': '"Windows"',
         }
     )
-    r.raise_for_status()
+    request.raise_for_status()
 
-    return r
+    return request
 
 
-radars = do_request(endpoint('all'))
-radars.raise_for_status()
+if __name__ == "__main__":
+    s = requests.Session()
+    s.mount('https://', HTTPAdapter(max_retries=5))
+    radars = do_request(s, endpoint('all'))
 
-with open('data/___all___.json', 'w') as f:
-    f.write(json.dumps(radars.json(), indent=4))
+    with open('data/___all___.json', 'w') as f:
+        f.write(json.dumps(radars.json(), indent=4))
 
-for radar in tqdm(radars.json()):
-    id = radar['id']
+    for radar in tqdm(radars.json()):
+        radar_id = radar['id']
 
-    r = do_request(endpoint(id))
-    r.raise_for_status()
+        r = do_request(endpoint(radar_id))
+        r.raise_for_status()
 
-    path = 'data/{id}.json'.format(id=id)
-    with open(path, 'w') as f:
-        f.write(json.dumps(r.json(), indent=4))
+        path = 'data/{radar_id}.json'.format(radar_id=radar_id)
+        with open(path, 'w') as f:
+            f.write(json.dumps(r.json(), indent=4))
